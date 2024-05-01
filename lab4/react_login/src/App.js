6e1a57a4b000f1c58603fc7970d49dd9a928e6fe
Jia Chiuan <b1129046@cgu.edu.tw>
2024-05-01 15:53:48 +0800

    lab4

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CGUlogin from './CGUlogin';
+
+function App() {
+  return (
+    <div className="App">
+      {CGUlogin()}
+    </div>
+  );
+}
+
+export default App;
